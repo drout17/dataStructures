1f6fcc3de949097c8cf83ccd361266c2bd6da799
drout17 <drout17@gmail.com>
2022-10-05 23:25:06 +0530

    Added Traversal

--- src/Tree.java
+++ src/Tree.java
@@ -1,21 +1,24 @@
 public class Tree {
-    static class Node{
+    static class Node {
         int data;
         Node left;
         Node right;
 
-        Node(int data){
-            this.data=data;
-            this.left=null;
-            this.right=null;
+        Node(int data) {
+            this.data = data;
+            this.left = null;
+            this.right = null;
         }
     }
 
-    static class BinaryTree{
-        static int index=-1;
-        public static Node buildTree(int nodes[]){
+    static class BinaryTree {
+        static int index = -1;
+        static int value = 0;
+
+        public static Node buildTree(int nodes[]) {
             index++;
-            if(nodes[index] == -1){
+            value = nodes[index];
+            if (nodes[index] == -1) {
                 return null;
             }
 
@@ -27,9 +30,40 @@ public class Tree {
     }
 
     public static void main(String[] args) {
-        int nodes[] = {1,2,4,-1,-1,5,-1,-1,3,-1,6,-1,-1};
+        int nodes[] = {1, 2, 4, -1, -1, 5, -1, -1, 3, -1, 6, -1, -1};
         BinaryTree tree = new BinaryTree();
         Node root = tree.buildTree(nodes);
-        System.out.println(root.data);
+        preOrder(root);
+        System.out.println(" ");
+        inOrder(root);
+        System.out.println(" ");
+        postOrder(root);
+    }
+
+    public static void preOrder(Node node){
+        if(node!=null)
+        {
+            System.out.print(node.data+" , ");
+            preOrder(node.left);
+            preOrder(node.right);
+        }
+        else
+            System.out.print(-1+" , ");
+    }
+
+    public static void inOrder(Node node){
+        if(node==null)
+            return;
+        inOrder(node.left);
+        System.out.print(node.data+" , ");
+        inOrder(node.right);
+    }
+
+    public static void postOrder(Node node){
+        if(node==null)
+            return;
+        postOrder(node.left);
+        postOrder(node.right);
+        System.out.print(node.data+" , ");
     }
 }
